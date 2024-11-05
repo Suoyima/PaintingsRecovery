0376457061930f5b07b14a085b0c9e877ebc14e2
suoyi <1165073607@qq.com>
2024-11-05 23:34:18 +0800

    第七次提交：修改比例，修改音乐文件名，添加音乐切换图标

--- js/main/script.js
+++ js/main/script.js
@@ -1,11 +1,14 @@
 function toggleMusic(){
-	var audio = document.getElementById("backgroundMusic");
-	if(audio.paused){
-		audio.play();
-	}
-	else{
-		audio.pause();
-	}
+    var audio = document.getElementById("backgroundMusic");
+    var button = document.getElementById("buttonMusic");
+    
+    if (audio.paused) {
+        audio.play();
+        button.src = "images/main/buttonMusicOn.png";
+    } else {
+        audio.pause();
+        button.src = "images/main/buttonMusicDown.png";
+    }
 }
 
 function navigateTo(pageUrl) {
