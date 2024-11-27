4b15abcac2cf6a1a6b8a57a95583365da7db4a60
RezeJoy <151029467+RezeJoy@users.noreply.github.com>
2024-11-27 19:08:56 +0800

    新增浇水效果和返回主界面

--- js/taskFirst/taskFirst.js
+++ js/taskFirst/taskFirst.js
@@ -4,3 +4,8 @@ var water = document.querySelector('.water');
 button.addEventListener('click', function(){
     water.style.opacity='1';
 })
+
+var back_button=document.querySelector('.close_button');
+back_button.addEventListener('click',function(){
+    window.location.href = 'mainPage.html';
+})
